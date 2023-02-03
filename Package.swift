fd7aa6b593b1763f0e892c92cc1e083d34c001b8
Richard L Zarth III <rlziii@icloud.com>
2023-02-03 09:51:43 -0600

    Change package name.

--- Package.swift
+++ Package.swift
@@ -1,13 +1,13 @@
 // swift-tools-version: 5.7
-
 import PackageDescription
 
 let package = Package(
     name: "swiftui-async-button",
     products: [
         .library(
-            name: "swiftui-async-button",
-            targets: ["swiftui-async-button"]),
+            name: "SwiftUIAsyncButton",
+            targets: ["swiftui-async-button"]
+        ),
     ],
     targets: [
         .target(
